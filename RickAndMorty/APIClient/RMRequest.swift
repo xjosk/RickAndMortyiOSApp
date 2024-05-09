9dc52e7f9314489ac5e0804b3102464756fdbd46
Andrei Carvajal <acarvajal@bakerrobotics.com>
2024-05-09 10:12:47 -0500

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Andrei E. Carvajal Brito on 09/05/24.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
---
+++ RickAndMorty/APIClient/RMService.swift
@@ -0,0 +1,25 @@
+//
+//  RMService.swift
+//  RickAndMorty
+//
+//  Created by Andrei E. Carvajal Brito on 09/05/24.
+//
+
+import Foundation
+
+/// Primary API service object to get Rick and Morty data
+final class RMService {
+    /// Shared singleton instance
+    static let shared = RMService()
+    
+    /// Privatized constructor
+    private init(){}
+    
+    /// Send Rick and Morty API Call
+    /// - Parameters:
+    ///     - request: Request instance
+    ///     - completion: Callback with data or error
+    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+        
+    }
+}
